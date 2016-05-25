337e2fb8811303aaa083b13c3538cb6e2feae688
Trevor Pierce <trevor.pierce@continuumdesign.net>
2016-05-24 22:58:14 -0500

    Changing the Sass build task to not stop the --watch task on error.

--- app/templates/tasks/sass/sassBuild.js
+++ app/templates/tasks/sass/sassBuild.js
@@ -1,31 +1,33 @@
 /* global module */
 module.exports = function(gulp, autoprefixer, stylefmt, browsersync, reload, $) {
-  return function() {
-    'use strict';
+    return function() {
+        'use strict';
 
-    var processors = [
-      autoprefixer({ browsers: ['last 2 versions'] }),
-      stylefmt()
-    ];
-    var filter = $.filter(['*.css', '!*.map'], { restore: true });
-    var stream = gulp.src('app/styles/sass/*.scss')
-    .pipe($.plumber())
-    .pipe($.sourcemaps.init())
-    .pipe($.sass({
-      errLogToConsole: true
-    }))
-    .pipe($.sourcemaps.write('./'))
-    .pipe(filter)
-    .pipe($.postcss(processors))
-    .pipe(filter.restore)
-    .pipe(gulp.dest('app/styles/css'))
-    .pipe(reload({stream: true}))
-    .pipe($.notify({
-      onLast: true,
-      message: 'Done concatenating CSS'
-    }));
+        var processors = [
+            autoprefixer({ browsers: ['last 2 versions'] }),
+            stylefmt()
+        ];
+        var filter = $.filter(['*.css', '!*.map'], { restore: true });
+        var stream = gulp.src('app/styles/sass/*.scss')
+            .pipe($.plumber())
+            .pipe($.sourcemaps.init())
+            .pipe($.sass())
+            .on('error', function(err) {
+              $.util.log(err.message);
+              this.emit('end');
+            })
+            .pipe($.sourcemaps.write('./'))
+            .pipe(filter)
+            .pipe($.postcss(processors))
+            .pipe(filter.restore)
+            .pipe(gulp.dest('app/styles/css'))
+            .pipe(reload({stream: true}))
+            .pipe($.notify({
+                onLast: true,
+                message: 'Done concatenating CSS'
+            }));
 
-    return stream;
-  };
+        return stream;
+    };
 };
 
